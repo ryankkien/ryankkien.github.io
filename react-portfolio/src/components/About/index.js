a0c083289863fd944660291566bd144e787e9f3c
ryankkien <93295033+ryankkien@users.noreply.github.com>
2024-07-18 20:43:30 -0700

    update to about me section

--- react-portfolio/src/components/About/index.js
+++ react-portfolio/src/components/About/index.js
@@ -64,17 +64,17 @@ const About = () => {
             />
           </h1>
           <p>
-            I'm a driven and passionate software engineer with a strong background in natural language processing, full stack development, and machine learning.
-            At the moment, I am looking for an internship as a software engineer in any of those fields to build up my experience and knowledge.
+            I'm a driven and passionate software engineer with a strong background in the interlay between neuroscience and AI, as well as web development. Please check out my past experiences, like my research internship at Osaka University which
+            I'm most proud of.
           </p>
           <p align="LEFT">
             I have a bachelor's degree in cognitive science w/ a specialization in computation from UCLA. Here, I spent much of my time in the Language Processing Lab and UCLA's
             League of Legends team. Another pride of mine is my victory at UCLA's DataFest 2022, where my team and I won the best insight award. I do want to continue pursuing my education
-            with either a masters or a PhD in computer science.
+            with either a masters or a PhD in computer science. 
           </p>
           <p>
-            If I were to describe myself in a sentence, it would be that I am always curious about everything.
-            Keep looking through the site to learn more about me! I will try to update it with things I find interesting.
+            Outside of coding I have a great passion for competitive video games and Japanese. Some achievments of mine in these categories is hitting top 300 out of the million North American League of Legends players and reaching highest level of Japanese language
+            proficiency (N1) when I pass my test this coming December. If you would like advice on League of Legends or tourist recommendations for Japan (I lived here for 6 months!) please let me know!
           </p>
         </div>
 
